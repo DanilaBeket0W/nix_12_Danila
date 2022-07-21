4dda30c60f7e6b823bf4486374823aa0738607fb
Danila <dan4ikbeket0w@gmail.com>
2022-07-21 11:27:24 +0300

    generics_HW_fix

--- src/main/java/ua/cars/CarsMain.java
+++ src/main/java/ua/cars/CarsMain.java
@@ -12,30 +12,3 @@ public class CarsMain {
         UIrun.start();
     }
 }
-
-class MyOptional<T extends Vehicle>{
-    private T value;
-
-    public MyOptional(T value){
-        this.value = value;
-    }
-
-    public T getValue() {
-        return value;
-    }
-
-    public void discountPrice(){
-        int randomProcent = 10 + (int) (Math.random() * 30);
-        BigDecimal price = value.getPrice();
-        BigDecimal discount = BigDecimal.valueOf((price.doubleValue()*100)/randomProcent);
-        price.subtract(discount);
-        value.setPrice(price);
-    }
-    public <K extends Number> void priceIncrease(K number){
-        value.setPrice(value.getPrice().multiply(BigDecimal.valueOf(number.doubleValue())));
-    }
-
-    public void printObj(){
-        System.out.println(value);
-    }
-}
